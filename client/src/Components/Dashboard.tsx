8658a13e557593a3df6fc7e5c9f2a7e45a70c639
Patrick Assaf <patrickassaf96@gmail.com>
2020-09-30 23:32:58 -0400

    Add buttons to change month and add new annual leave

--- client/src/Components/Dashboard.tsx
+++ client/src/Components/Dashboard.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Message } from './Message';
+import Button from 'react-bootstrap/Button';
 
 interface Props {
 
 }
 
 export const Dashboard: React.FC<Props> = () => {
+
+    const [month, setMonth] = useState("October");
+
     return (
         <div className="feature-box">
             <p className="feature-title">
-                Annual Leave
+                {month} 2020 - Annual Leave 
+                    <Button variant="outline-primary" size="sm" className="ml-3" onClick={(e) => month === "October" ? setMonth("November") : setMonth("October")}>
+                        Change Month
+                    </Button>
             </p>
+            <div className="d-flex">
+                <Button variant="primary" className="ml-auto feature-button"><span className="pr-2">+</span> Add annual leave</Button>
+            </div>
             <Message />
         </div>
     );
