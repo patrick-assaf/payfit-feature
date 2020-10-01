65329e7fcbd1882814612321b4979d13a82b789f
Patrick Assaf <patrickassaf96@gmail.com>
2020-10-01 11:17:20 -0400

    Adjust display and styling of message component

--- client/src/Components/Message.tsx
+++ client/src/Components/Message.tsx
@@ -15,30 +15,42 @@ export const Message: React.FC<Props> = ({format, content}: Props) => {
     if(format === "info") {
         return (
             <div className="message-box info">
-                <FontAwesomeIcon icon={faInfoCircle} style={{color: "gray"}} size="lg" />
-                <p className="message-text">
-                    {content}
-                </p>
+                <div className="message-text">
+                    <span className="pr-3">
+                        <FontAwesomeIcon icon={faInfoCircle} style={{color: "gray"}} size="lg" className="d-inline" />
+                    </span>
+                    <p className="d-inline">
+                        {content}
+                    </p>
+                </div>
             </div> 
         );
     }
     if(format === "success") {
         return (
             <div className="message-box success">
-                <FontAwesomeIcon icon={faCheckCircle} style={{color: "#00c800b3"}} size="lg" />
-                <p className="message-text">
-                    {content}
-                </p>
+                <div className="message-text">
+                    <span className="pr-3">
+                        <FontAwesomeIcon icon={faCheckCircle} style={{color: "#00c800b3"}} size="lg" className="d-inline" />
+                    </span>
+                    <p className="d-inline">
+                        {content}
+                    </p>
+                </div>
             </div>
         );
     }
     if(format === "warning") {
         return (
             <div className="message-box warning">
-                <FontAwesomeIcon icon={faTimesCircle} style={{color: "#ff0000b3"}} size="lg" />
-                <p className="message-text">
-                    {content}
-                </p>
+                <div className="message-text">
+                    <span className="pr-3">
+                        <FontAwesomeIcon icon={faTimesCircle} style={{color: "#ff0000b3"}} size="lg" className="d-inline" />
+                    </span>
+                    <p className="d-inline">
+                        {content}
+                    </p>
+                </div>
             </div>
         ); 
     }
