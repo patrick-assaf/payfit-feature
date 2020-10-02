b3a31695be6005bdcf25fec7ec442ba0a6d14e00
Patrick Assaf <patrickassaf96@gmail.com>
2020-10-01 23:57:31 -0400

    Bring up modal on button press

--- client/src/Components/Dashboard.tsx
+++ client/src/Components/Dashboard.tsx
@@ -3,6 +3,7 @@ import { Message } from './Message';
 import { HeaderBar } from './HeaderBar';
 import { LeaveTable } from './LeaveTable';
 import Button from 'react-bootstrap/Button';
+import Popup from 'reactjs-popup';
 
 interface Props {
 
@@ -11,6 +12,7 @@ interface Props {
 export const Dashboard: React.FC<Props> = () => {
 
     return (
+        <>
         <div className="feature-box shadow">
             <p className="feature-title">
                 October 2020 - Annual Leave
@@ -18,9 +20,14 @@ export const Dashboard: React.FC<Props> = () => {
             <HeaderBar taken={0} />
             <LeaveTable />
             <div className="d-flex">
+            <Popup trigger={ 
                 <Button variant="primary" className="ml-auto feature-button pt-2 pb-2 mb-3"><span className="pr-2">+</span> Add annual leave</Button>
+            } modal>
+                <div>Modal content</div>
+            </Popup>
             </div>
             <Message format="info" content="Your annual leave runs from 01/01/2020 to 31/12/2020." />
         </div>
+        </>
     );
 };
