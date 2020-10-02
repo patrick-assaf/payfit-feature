364313b1d2bbe3469e7032b5fcf912369cbe6d15
Patrick Assaf <patrickassaf96@gmail.com>
2020-10-01 23:42:15 -0400

    Clean up initial page elements set up

--- client/src/Components/Dashboard.tsx
+++ client/src/Components/Dashboard.tsx
@@ -18,11 +18,9 @@ export const Dashboard: React.FC<Props> = () => {
             <HeaderBar taken={0} />
             <LeaveTable />
             <div className="d-flex">
-                <Button variant="primary" className="ml-auto feature-button"><span className="pr-2">+</span> Add annual leave</Button>
+                <Button variant="primary" className="ml-auto feature-button pt-2 pb-2 mb-3"><span className="pr-2">+</span> Add annual leave</Button>
             </div>
             <Message format="info" content="Your annual leave runs from 01/01/2020 to 31/12/2020." />
-            <Message format="warning" content="This is a warning message!" />
-            <Message format="success" content="This is a success message." />
         </div>
     );
 };
