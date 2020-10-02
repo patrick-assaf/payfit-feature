dcb9faaca9b9d198eff6596d63e3770c29ac75d7
Patrick Assaf <patrickassaf96@gmail.com>
2020-10-02 12:17:53 -0400

    Create a Modal component

--- client/src/Components/Dashboard.tsx
+++ client/src/Components/Dashboard.tsx
@@ -4,6 +4,7 @@ import { HeaderBar } from './HeaderBar';
 import { LeaveTable } from './LeaveTable';
 import Button from 'react-bootstrap/Button';
 import Popup from 'reactjs-popup';
+import { Modal } from './Modal';
 
 interface Props {
 
@@ -23,7 +24,7 @@ export const Dashboard: React.FC<Props> = () => {
             <Popup trigger={ 
                 <Button variant="primary" className="ml-auto feature-button pt-2 pb-2 mb-3"><span className="pr-2">+</span> Add annual leave</Button>
             } modal>
-                <div>Modal content</div>
+                <Modal />
             </Popup>
             </div>
             <Message format="info" content="Your annual leave runs from 01/01/2020 to 31/12/2020." />
---
+++ client/src/Components/Modal.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+export const Modal: React.FC<Props> = () => {
+    return (
+        <div>Modal content</div>
+    );
+};
