88ee1f72297343361a4734068265cdaef6cc605b
Patrick Assaf <patrickassaf96@gmail.com>
2020-10-01 23:36:06 -0400

    Add table elements for when table is empty

--- client/src/Components/LeaveTable.tsx
+++ client/src/Components/LeaveTable.tsx
@@ -7,15 +7,21 @@ interface Props {
 
 export const LeaveTable: React.FC<Props> = () => {
     return (
-        <div className="header-box row text-left">
-            <div className="col-sm header-title">
-                <div>Days off</div>
+        <div className="table-box">
+            <div className="row">
+                <div className="col-sm header-title">
+                    <div>Days off</div>
+                </div>
+                <div className="col-sm header-title">
+                    <div>From</div>
+                </div>
+                <div className="col-sm header-title">
+                    <div>To</div>
+                </div>
             </div>
-            <div className="col-sm header-title">
-                <div>From</div>
-            </div>
-            <div className="col-sm header-title">
-                <div>To</div>
+            <hr></hr>
+            <div className="text-center">
+                <p className="table-empty">No element</p>
             </div>
         </div>
     );
