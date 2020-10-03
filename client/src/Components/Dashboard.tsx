0046397c20b4c0cecaabb7c7d6a89857138015d6
Patrick Assaf <patrickassaf96@gmail.com>
2020-10-02 22:31:45 -0400

    Add a Modal component and set appropriate props

--- client/src/Components/Dashboard.tsx
+++ client/src/Components/Dashboard.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Message } from './Message';
 import { HeaderBar } from './HeaderBar';
 import { LeaveTable } from './LeaveTable';
 import Button from 'react-bootstrap/Button';
-import Popup from 'reactjs-popup';
-import { Modal } from './Modal';
+import Modal from 'react-bootstrap/Modal';
 
 interface Props {
 
 }
 
 export const Dashboard: React.FC<Props> = () => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
         <>
@@ -21,14 +25,36 @@ export const Dashboard: React.FC<Props> = () => {
             <HeaderBar taken={0} />
             <LeaveTable />
             <div className="d-flex">
-            <Popup trigger={ 
-                <Button variant="primary" className="ml-auto feature-button pt-2 pb-2 mb-3"><span className="pr-2">+</span> Add annual leave</Button>
-            } modal>
-                <Modal />
-            </Popup>
+                <Button onClick={handleShow} variant="primary" className="ml-auto feature-button pt-2 pb-2 mb-3"><span className="pr-2">+</span> Add annual leave</Button>
             </div>
             <Message format="info" content="Your annual leave runs from 01/01/2020 to 31/12/2020." />
         </div>
+
+        <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>Modal title</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                I will not close if you click outside me. Don't even try to press
+                escape key.
+            </Modal.Body>
+            
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary">Understood</Button>
+            </Modal.Footer>
+        </Modal>
         </>
     );
 };
