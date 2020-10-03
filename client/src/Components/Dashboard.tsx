adf3bc1b01fd60901d797b251875797d6f43ac64
Patrick Assaf <patrickassaf96@gmail.com>
2020-10-03 01:56:15 -0400

    Include form elements and labels in modal

--- client/src/Components/Dashboard.tsx
+++ client/src/Components/Dashboard.tsx
@@ -4,6 +4,12 @@ import { HeaderBar } from './HeaderBar';
 import { LeaveTable } from './LeaveTable';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'; 
 
 interface Props {
 
@@ -44,9 +50,22 @@ export const Dashboard: React.FC<Props> = () => {
             </Modal.Header>
 
             <Modal.Body>
-                <div className="text-center modal-body">
-                    Modal content
-                </div>
+                <Form className="d-flex justify-content-center" inline>
+                <Form.Group>
+                    <Form.Label htmlFor="startDate" className="label-text pr-4 pb-2">Start of leave*</Form.Label>
+                    <InputGroup className="mb-2 mr-sm-2" id="startDate">
+                        <InputGroup.Prepend>
+                        <InputGroup.Text>
+                            <FontAwesomeIcon icon={faCalendarAlt} style={{color: "gray"}} className="d-inline" />
+                        </InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl placeholder="Pick a date" />
+                    </InputGroup>
+                    <Form.Text className="pl-2 pb-3" muted>
+                        <FontAwesomeIcon icon={faInfoCircle} style={{color: "gray"}} className="d-inline mr-1"/> Must be 8-20 characters long.
+                    </Form.Text>
+                </Form.Group>
+                </Form>
             </Modal.Body>
 
             <Message format="info" content="Choose the type of leave that you wish to add." />
