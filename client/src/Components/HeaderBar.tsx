662c84e5958e09e7ce481be95485fdf248be9df9
Patrick Assaf <patrickassaf96@gmail.com>
2020-10-04 02:52:22 -0400

    Display number of monthly leave days in the dashboard header

--- client/src/Components/HeaderBar.tsx
+++ client/src/Components/HeaderBar.tsx
@@ -10,6 +10,16 @@ export const HeaderBar: React.FC<Props> = ({leaves, daysLeft}: Props) => {
 
     let daysThisMonth: any = 0;
 
+    const is_weekend = (dateChecked: any) => {
+        let date = new Date(dateChecked);
+        if(date.getDay() === 5 || date.getDay() === 6) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     const monthDays = () => {
         let days = daysThisMonth;
         // eslint-disable-next-line
@@ -18,11 +28,54 @@ export const HeaderBar: React.FC<Props> = ({leaves, daysLeft}: Props) => {
             let startDate = new Date(leaves[leaveKey].start);
             let endMonth = endDate.getMonth();
             let startMonth = startDate.getMonth();
-            if((endMonth === 9 || startMonth === 9) && leaves[leaveKey].type === "Paid Vacation") {
+            if((endMonth === 9 && startMonth === 9) && leaves[leaveKey].type === "Paid Vacation") {
                 days += leaves[leaveKey].daysTaken;
             }
-            else {
-                days += 0;
+            else if((startMonth < 9 && endMonth === 9) && leaves[leaveKey].type === "Paid Vacation") {
+                let end = leaves[leaveKey].end;
+                let year = end.slice(0, end.search("-"));
+                let dateCut = end.slice(end.search("-")+1);
+                let month = dateCut.slice(0, dateCut.search("-"))
+
+                let currentDate = year + "-" + month + "-01";
+                let startDay = 1;
+
+                while(currentDate !== end) {
+                    if(!is_weekend(currentDate)) {
+                        days += 1;
+                    }
+                    startDay += 1;
+                    let startDayPrint = startDay < 10 ? '0' + startDay : startDay;
+                    currentDate = year + "-" + month + "-" + startDayPrint;
+                }
+
+                if(!is_weekend(end)) {
+                    days += 1;
+                }
+            }
+            else if((startMonth === 9 && endMonth > 9) && leaves[leaveKey].type === "Paid Vacation") {                    
+                let start = leaves[leaveKey].start;
+                let year = start.slice(0, start.search("-"));
+                let dateCut = start.slice(start.search("-")+1);
+                let month = dateCut.slice(0, dateCut.search("-"));
+                let startDay = dateCut.slice(dateCut.search("-")+1);
+
+                let lastMonthDay = new Date(year, month, 0).getDate();
+
+                let currentDate = start;
+                let daysCount = parseInt(startDay);
+
+                startDay = parseInt(startDay);
+
+                while(daysCount <= lastMonthDay) {
+                    if(!is_weekend(currentDate)) {
+                        days += 1;
+                    }
+                    daysCount += 1;
+                    startDay += 1;
+                    let startDayPrint = startDay < 10 ? '0' + startDay : startDay;
+                    currentDate = year + "-" + month + "-" + startDayPrint;
+                }
             }
         });
         return days;
